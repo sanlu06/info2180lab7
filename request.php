d042924529661f04a4b62f39f26257da541793eb
pigeonflight <david@alteroo.com>
2014-11-07 10:15:22 -0500

    initial commit

---
+++ request.php
@@ -0,0 +1,11 @@
+<?php
+// For PHP 5 and up
+/* $term = $_REQUEST['term'];
+$all = $_REQUEST['all'];
+$author = $_REQUEST['author'];
+$handle = fopen("https://webster.cs.washington.edu/cse154/labs/ajax/urban.php?term=$term&all=$all&author=$author", "rb");
+$contents = stream_get_contents($handle);
+print $contents;
+fclose($handle); */
+echo "hello I'm request.php";
+?>
