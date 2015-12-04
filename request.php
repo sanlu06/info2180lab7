192d2e6397b00643ff7b84f82cf5f7a876954858
sanlu <intelec2alsanlu@gmail.com>
2015-12-04 20:04:46 +0000

    files

--- request.php
+++ request.php
@@ -3,9 +3,36 @@
 
 # accept a term (keyword)
 # respond with a value
-$query = $_REQUEST['q'];
+#$query = $_REQUEST['q'];
+header("Content-type: text/xml");
+print("");
 
-$definition = [
+
+$definition = <<<XML
+<entries>
+
+    <definition name="definition">
+
+        a statement of the exact meaning of a word,especially in a dictionary.
+
+    </definition>
+
+    <definition name="bar">
+
+        a place that sells alcholic beverages.
+
+    </definition>
+
+    <definition name="ajax">
+
+        technique which involves the use of javascript 
+
+    </definition>
+
+</entries>
+XML;
+
+/*$definition = [
     "definition" => "a statement of the exact meaning of a word, especially in a dictionary.",
     "bar" => "a place that sells alcholic beverages",
     "ajax" => "technique which involves the use of javascript and xml"
@@ -13,6 +40,9 @@ $definition = [
 
 print $query;
 print "<br>";
-print($definition[$query]);
+print($definition[$query]);*/
 
+$xml = new SimpleXMLElement($definition);
+echo $xml->asXML();
 ?>
+
