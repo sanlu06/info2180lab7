f509d9cbd51369240861661228f331bbb442458f
sanlu06 <intelec2alsanlu@gmail.com>
2015-12-04 15:10:46 -0500

    Update urban.js

--- urban.js
+++ urban.js
@@ -1,28 +1,76 @@
    window.onload = function() {
-           new Ajax.Request(
-               "https://phpajax-c9-pigeonflight.c9.io/request.php",
-              {
-                  parameters: {term:"API"},
-                   method: "get",
-                   onSuccess: naseberry //callback is a function that collects 
-                                       //the server payload (data)
-                }
+             document.getElementById("search").addEventListener("click", function(){
+                // go out and get result from request.php
+                var a,b,c,x;
+                 a = "request.php?q=&all=true";
+                 b = document.getElementById("searchterm").value;
+                 c = a.concat(b);
+                //creates an ordered list on results
+                 x = document.createElement("OL");
+                var list = document.getElementById("result");
+                document.getElementById("result").appendChild(a);
+                 list.insertBefore(x, list.childNodes[0]);
+                
+                
+                
+                new Ajax.Request(c,
+                       {
+                          parameters: {term:"API"},
+                          method: "get",
+                          onSuccess: showDefinition,
+                         // onFailure: showDefinition
+                         });
            
-            );
-            }  
-            function naseberry(data){
-                alert(data.responseText);
-                // data.responseText, data.responseXML, data.status
-            }
-            
-/*         new Ajax.Request("https://developers.google.com/speed/libraries/devguide",
-               {
-                  method: "get",
-                  onSuccess: doSomethingWithTheText
-              });
+function showDefinition(data) {
+              
+                document.write (data.responseText);
+                        }
+           });
+};
+
+/* window.onload = function() {
+             document.getElementById("search").addEventListener(
+                 
+                 "click", function(){
+                     
+                // go out and get result from request.php
+                var a = "request.php?q=";
+                var b = document.getElementById("searchterm").value;
+                var c = a.concat(b);
+                
+                //creates an ordered list on results
+               /* var x = document.createElement("OL");
+                //var list = document.getElementById("result");
+                //x.appendChild(a);
+                //document.body.appendChild(n);
+                document.getElementById("result").appendChild(a);
+                 list.insertBefore(x, list.childNodes[0]);
+                
+                
+                 new Ajax.Request(c,
+                       {
+                          //parameters: {term:"API"},
+                          method: "get",
+                          onSuccess: showDefinition
+                         });
+           });
+
+function showDefinition(data) {
+              
+                document.write (data.responseText);
+                        }
            };
-*/
-function doSomethingWithTheText(data) {
-  alert(data.responseText);
+
+function myFunction() {
+     var newItem = document.createElement("OL");
+                var textnode = document.createTextNode("Water");
+                newItem.appendChild(list);
+                
+                var list = document.getElementById("a");
+                list.insertBefore(newItem, list.childNodes[0]);
 }
+  */  
+           
+               
+     
                
